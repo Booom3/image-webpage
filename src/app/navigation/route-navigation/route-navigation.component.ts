040cc9bce169b68a17849940563bb443792f6d6d
Booom3 <joakim241@hotmail.com>
2017-08-05 15:28:56 +0200

    Updated in-route navigation to data binding
    This also fixes the issue where routerLinkActive would not update
    I don't know why it fixes it

--- src/app/navigation/route-navigation/route-navigation.component.ts
+++ src/app/navigation/route-navigation/route-navigation.component.ts
@@ -1,6 +1,11 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 
+class Tab {
+  name: string;
+  link: string[];
+}
+
 @Component({
   selector: 'app-route-navigation',
   templateUrl: './route-navigation.component.html',
@@ -9,11 +14,16 @@ import { ActivatedRoute } from '@angular/router';
 export class RouteNavigationComponent implements OnInit, OnDestroy {
   private sub: any;
   public route_id: any;
+  public tabs: Tab[];
   constructor(public route: ActivatedRoute) { }
 
   ngOnInit() {
     this.sub = this.route.params.subscribe(params => {
       this.route_id = params.route_id;
+      this.tabs = [
+        { name: 'User', link: ['/user', this.route_id] },
+        { name: 'Upload', link: ['/upload', this.route_id ] }
+      ]
     });
   }
 
